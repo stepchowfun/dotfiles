79d3675aa7190868cd45500b53f345b0addd1e2e
Stephan Boyer <stephan@stephanboyer.com>
2021-10-23 22:37:53 -0700

    Fix the installer script

--- install-local.sh
+++ install-local.sh
@@ -1,36 +1,39 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-DIR="$(cd "$(dirname "$0")" && pwd)"
+# Make Bash not silently ignore errors.
+set -euo pipefail
 
+# Check for Debian/Ubuntu.
 if uname -a | grep -qi 'Debian\|Ubuntu'; then
   echo 'Debian or Ubuntu detected.'
+  export DEBIAN_FRONTEND=noninteractive
 
   echo 'Updating package lists...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get -y update < /dev/tty
+  sudo apt-get -y update < /dev/tty
 
   echo 'Installing Alacritty...'
-  DEBIAN_FRONTEND=noninteractive sudo add-apt-repository ppa:mmstick76/alacritty
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y alacritty < /dev/tty
+  sudo add-apt-repository ppa:mmstick76/alacritty
+  sudo apt-get install -y alacritty < /dev/tty
 
   echo 'Installing Git...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y git < /dev/tty
+  sudo apt-get install -y git < /dev/tty
 
   echo 'Installing ripgrep...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y ripgrep < /dev/tty
+  sudo apt-get install -y ripgrep < /dev/tty
 
   echo 'Installing zsh...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y zsh < /dev/tty
+  sudo apt-get install -y zsh < /dev/tty
 
   echo 'Setting the login shell to zsh...'
   sudo chsh -s "$(which zsh)" "$(whoami)" < /dev/tty
 
   echo 'Installing oh-my-zsh...'
-  rm -rf "~/.oh-my-zsh"
+  rm -rf ~/.oh-my-zsh
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   echo 'Installing the `Input` font...'
   mkdir -p ~/.local/share/fonts
-  cp "$DIR/input-font/Input_Fonts/Input/"* ~/.local/share/fonts
+  cp "input-font/Input_Fonts/Input/"* ~/.local/share/fonts
 
   if which fc-cache >/dev/null 2>&1; then
     echo "Resetting font cache..."
@@ -38,42 +41,45 @@ if uname -a | grep -qi 'Debian\|Ubuntu'; then
   fi
 
   echo 'Installing tmux...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y tmux < /dev/tty
+  sudo apt-get install -y tmux < /dev/tty
 
   echo 'Installing neovim...'
-  DEBIAN_FRONTEND=noninteractive sudo apt-get install -y neovim python3-neovim < /dev/tty
+  sudo apt-get install -y neovim python3-neovim < /dev/tty
 
   echo 'Installing vim-plug...'
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
   echo 'Downloading submodules...'
-  (cd "$DIR" && git submodule update --init)
+  git submodule update --init
 
   echo 'Installing dotfiles...'
-  cp  "$DIR/.tmux.conf" ~/.tmux.conf
-  cp  "$DIR/.zshrc" ~/.zshrc
+  cp  ".tmux.conf" ~/.tmux.conf
+  cp  ".zshrc" ~/.zshrc
   rm -rf ~/.config/base16-shell
   mkdir -p ~/.config/base16-shell
-  cp -r "$DIR/.config/base16-shell" ~/.config
+  cp -r ".config/base16-shell" ~/.config
   rm -rf ~/.config/nvim
   mkdir -p ~/.config/nvim
-  cp -r "$DIR/.config/nvim" ~/.config
+  cp -r ".config/nvim" ~/.config
   rm -rf ~/.config/alacritty
   mkdir -p ~/.config/alacritty
-  cp -r "$DIR/.config/alacritty" ~/.config
+  cp -r ".config/alacritty" ~/.config
 
   echo 'Installing vim plugins...'
   nvim -c PlugInstall -c PlugUpdate -c qa
 
   echo 'Installing color scheme...'
-  cp "$DIR/base16-circus-scheme/circus/scripts/base16-circus.sh" ~/.config/base16-shell/scripts
-  cp "$DIR/base16-circus-scheme/circus/colors/base16-circus.vim" ~/.local/share/nvim/plugged/base16-vim/colors
+  cp "base16-circus-scheme/circus/scripts/base16-circus.sh" \
+    ~/.config/base16-shell/scripts
+  cp "base16-circus-scheme/circus/colors/base16-circus.vim" \
+    ~/.local/share/nvim/plugged/base16-vim/colors
 
   echo 'Patching the vim color scheme to not set the background color...'
   echo 'This allows vim to use the background set by tmux, which is configured'
   echo 'to use a lighter background for panes that are not in focus.'
-  sed -E -i.bak 's/[ \t]*let[ \t]+s:cterm00[ \t]*=.*$/let s:cterm00 = "none"/' ~/.local/share/nvim/plugged/base16-vim/colors/base16-circus.vim
+  sed -E -i.bak 's/[ \t]*let[ \t]+s:cterm00[ \t]*=.*$/let s:cterm00 = "none"/' \
+    ~/.local/share/nvim/plugged/base16-vim/colors/base16-circus.vim
 
   echo 'Setting base16-shell color scheme...'
   zsh -ic base16_circus
@@ -85,6 +91,7 @@ if uname -a | grep -qi 'Debian\|Ubuntu'; then
   exit
 fi
 
+# Check for macOS.
 if uname -a | grep -qi 'Darwin'; then
   echo 'macOS detected.'
 
@@ -112,11 +119,11 @@ if uname -a | grep -qi 'Darwin'; then
   sudo chsh -s "$(which zsh)" "$(whoami)" < /dev/tty
 
   echo 'Installing oh-my-zsh...'
-  rm -rf "~/.oh-my-zsh"
+  rm -rf ~/.oh-my-zsh
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   echo 'Installing the `Input` font...'
-  cp "$DIR/input-font/Input_Fonts/Input/"* ~/Library/Fonts
+  cp "input-font/Input_Fonts/Input/"* ~/Library/Fonts
 
   echo 'Installing tmux...'
   brew install tmux
@@ -140,32 +147,35 @@ if uname -a | grep -qi 'Darwin'; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
   echo 'Downloading submodules...'
-  (cd "$DIR" && git submodule update --init)
+  git submodule update --init
 
   echo 'Installing dotfiles...'
-  cp  "$DIR/.tmux.conf" ~/.tmux.conf
-  cp  "$DIR/.zshrc" ~/.zshrc
+  cp  ".tmux.conf" ~/.tmux.conf
+  cp  ".zshrc" ~/.zshrc
   rm -rf ~/.config/base16-shell
   mkdir -p ~/.config/base16-shell
-  cp -r "$DIR/.config/base16-shell" ~/.config
+  cp -r ".config/base16-shell" ~/.config
   rm -rf ~/.config/nvim
   mkdir -p ~/.config/nvim
-  cp -r "$DIR/.config/nvim" ~/.config
+  cp -r ".config/nvim" ~/.config
   rm -rf ~/.config/alacritty
   mkdir -p ~/.config/alacritty
-  cp -r "$DIR/.config/alacritty" ~/.config
+  cp -r ".config/alacritty" ~/.config
 
   echo 'Installing vim plugins...'
   nvim -c PlugInstall -c qa
 
   echo 'Installing color scheme...'
-  cp "$DIR/base16-circus-scheme/circus/scripts/base16-circus.sh" ~/.config/base16-shell/scripts
-  cp "$DIR/base16-circus-scheme/circus/colors/base16-circus.vim" ~/.local/share/nvim/plugged/base16-vim/colors
+  cp "base16-circus-scheme/circus/scripts/base16-circus.sh" \
+    ~/.config/base16-shell/scripts
+  cp "base16-circus-scheme/circus/colors/base16-circus.vim" \
+    ~/.local/share/nvim/plugged/base16-vim/colors
 
   echo 'Patching the vim color scheme to not set the background color...'
   echo 'This allows vim to use the background set by tmux, which is configured'
   echo 'to use a lighter background for panes that are not in focus.'
-  sed -E -i.bak 's/[ \t]*let[ \t]+s:cterm00[ \t]*=.*$/let s:cterm00 = "none"/' ~/.local/share/nvim/plugged/base16-vim/colors/base16-circus.vim
+  sed -E -i.bak 's/[ \t]*let[ \t]+s:cterm00[ \t]*=.*$/let s:cterm00 = "none"/' \
+    ~/.local/share/nvim/plugged/base16-vim/colors/base16-circus.vim
 
   echo 'Setting base16-shell color scheme...'
   zsh -ic base16_circus
--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 # We wrap everything in parentheses to prevent the shell from executing only a prefix of the script
-# if the download is interrupted.
+# if the download is interrupted and to prevent directory changes (`cd`) from persisting in the
+# shell after the script has finished.
 (
   rm -rf /tmp/dotfiles && \
   git clone https://github.com/stepchowfun/dotfiles.git /tmp/dotfiles && \
-  /tmp/dotfiles/install-local.sh && \
+  cd /tmp/dotfiles && \
+  ./install-local.sh && \
   rm -rf /tmp/dotfiles
 )
